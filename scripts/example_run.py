bcbc78657765780345dba12056d40b9a3f99e9f0
Colin Josey <cjosey@mit.edu>
2017-06-11 15:28:52 -0400

    Update run script

--- scripts/example_run.py
+++ scripts/example_run.py
@@ -6,31 +6,40 @@ import sys
 
 import example_geometry
 
-# Load geometry from example
-geometry, lower_left, upper_right = example_geometry.generate_cheap_geometry(n_rings=10)
+t_end = 150.0 * 24 * 3600 # 150 days, corresponding to peak l2 norm
+algorithm = opendeplete.integrator.celi_m1
+stages = 2
 
-# Create dt vector, 4 hour time steps for first 6 days, then 1 day time steps for 200 days.
-dt_1 = np.repeat(3600.0 * 4, 24)
-dt_2 = np.repeat(3600.0 * 24, 194)
-dt = np.append(dt_1, dt_2)
+neutron_grid = [500, 1000, 2000]
+step_grid = [1, 2, 3, 5, 6, 10, 15, 25]
 
-# Create settings variable
-settings = opendeplete.OpenMCSettings()
+for neutron in neutron_grid:
+    for step in step_grid:
+        folder_name = algorithm.__name__ + "/" + str(neutron) + "/" + str(step) + "/" + sys.argv[1]
 
-settings.openmc_call = "openmc"
-# An example for mpiexec:
-# settings.openmc_call = ["mpiexec", "openmc"]
-settings.particles = 30000
-settings.batches = 40
-settings.inactive = 20
-settings.lower_left = lower_left
-settings.upper_right = upper_right
+        # Load geometry from example
+        geometry, lower_left, upper_right = example_geometry.generate_cheap_geometry(n_rings=10)
 
-settings.power = 2.337e15*4  # MeV/second cm from CASMO
-settings.dt_vec = dt
-settings.output_dir = 'reference' + sys.argv[1]
+        # Time grid
+        dt = np.repeat(t_end / step, step)
 
-op = opendeplete.OpenMCOperator(geometry, settings)
+        # Create settings variable
+        settings = opendeplete.OpenMCSettings()
 
-# Perform simulation using the MCNPX/MCNP6 algorithm
-opendeplete.integrator.celi_m1(op)
+        neutron_actual = neutron * (4 / stages) * (step_grid[-1] / step)
+
+        settings.openmc_call = "openmc"
+        settings.particles = int(neutron_actual)
+        settings.batches = 40
+        settings.inactive = 20
+        settings.lower_left = lower_left
+        settings.upper_right = upper_right
+
+        settings.power = 2.337e15  # MeV/second cm from CASMO
+        settings.dt_vec = dt
+        settings.output_dir = folder_name
+
+        op = opendeplete.OpenMCOperator(geometry, settings)
+
+        # Perform simulation using the MCNPX/MCNP6 algorithm
+        opendeplete.integrator.celi_m1(op)
