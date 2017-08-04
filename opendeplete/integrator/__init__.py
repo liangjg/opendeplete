e2c7984e5f95159abbe62a4416589811429f0ce5
Colin Josey <cjosey@mit.edu>
2017-08-04 17:58:56 -0400

    celi imp

--- opendeplete/integrator/__init__.py
+++ opendeplete/integrator/__init__.py
@@ -8,13 +8,17 @@ The integrator subcomponents.
 from .cecm import *
 from .celi_m1 import *
 from .celi_cfq4 import *
+from .celi_cfq4_imp import *
+from .cf3_DIRK import *
 from .cf4 import *
 from .cram import *
 from .el3 import *
 from .el3_ats import *
+from .el3_stab import *
 from .epc_rk4 import *
 from .leqi_m1 import *
 from .leqi_cfq4 import *
+from .leqi_cfq4_imp import *
 from .predictor import *
 from .save_results import *
 from .sie import *
