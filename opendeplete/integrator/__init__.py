df509af82bc6e7a1094b6efebd18d09e60377084
Colin Josey <cjosey@mit.edu>
2017-08-04 21:56:22 -0400

    LEQI IMP

--- opendeplete/integrator/__init__.py
+++ opendeplete/integrator/__init__.py
@@ -9,7 +9,7 @@ from .cecm import *
 from .celi_m1 import *
 from .celi_cfq4 import *
 from .celi_cfq4_imp import *
-from .cf3_DIRK import *
+# from .cf3_DIRK import *
 from .cf4 import *
 from .cram import *
 from .el3 import *
